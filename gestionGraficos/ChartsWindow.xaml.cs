a58476b3fbcc969fe49d999819c7a58f173e4fa3
Oscarvicente@usal.es <Oscarvicente@usal.es>
2020-12-08 19:30:44 +0100

    2020/12/08 1930 Bar Chart Visualized ( 1 Bug)

--- gestionGraficos/ChartsWindow.xaml.cs
+++ gestionGraficos/ChartsWindow.xaml.cs
@@ -27,6 +27,7 @@ namespace Bricklin_App
         Polyline polyline;
 
         SortedDictionary<double, double> actualPolylineData = null;
+        SortedDictionary<double, double> actualBarData = null;
 
         public MainWindow()
         {
@@ -45,6 +46,12 @@ namespace Bricklin_App
 
 
             polylineCanvas.Children.Add(polyline);
+
+            actualPolylineData = Model.getInstance().getDataset().getData();
+            actualBarData = Model.getInstance().getDataset().getData();
+
+            createPolylineChart();
+            createBarChart();
 
             //ThemeManager.Current.ChangeTheme(this, "Light.Red");
         }
@@ -65,7 +72,8 @@ namespace Bricklin_App
                     createPolylineChart();
                     break;
                 case "Barras":
-                    //Bar chart stuff
+                    actualBarData = Model.getInstance().getDataset().getData();
+                    createBarChart();
                     break;
                 default:
                     //Mostrar error
@@ -122,9 +130,78 @@ namespace Bricklin_App
 
         }
 
+        private void createBarChart()
+        {
+            
+            SortedDictionary<double, double> data = actualBarData;
+
+            if (data == null)
+                return;
+
+            int numPuntos = (int)barCanvas.ActualWidth;
+
+
+            double xReal, yReal, xRealMax, xRealMin, yRealMax, yRealMin;
+
+            /*
+             * los valores y max y min se deben añadir a la clase Dataset
+             * */
+            xRealMax = data.Keys.Max();
+            xRealMin = data.Keys.Min();
+            yRealMax = data.Values.Max();
+            yRealMin = data.Values.Min();
+
+            double xPant, yPant, xPantMin, xPantMax, yPantMin, yPantMax;
+
+
+            xPantMin = 0;
+            xPantMax = barCanvas.ActualWidth;
+            yPantMin = 0;
+            yPantMax = barCanvas.ActualHeight;
+
+            int pointsRange = numPuntos / data.Count;
+
+            List<Line> lineList = new List<Line>();
+
+            double ypantallaEjeX = (yPantMin - yPantMax) * (0 - yRealMin) / (yRealMax - yRealMin) + yPantMax;
+
+            barCanvas.Children.Clear();
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                //xReal = xRealMin + (i*pointsRange) * (xRealMax - xRealMin) / numPuntos;
+                xReal = data.ElementAt(i).Key;
+                yReal = data.ElementAt(i).Value;
+
+                xPant = (xPantMax - xPantMin) * (xReal - xRealMin) / (xRealMax - xRealMin) + xPantMin;
+                yPant = (yPantMin - yPantMax) * (yReal - yRealMin) / (yRealMax - yRealMin) + yPantMax;              
+
+                Line line = new Line();
+                line.Stroke = Brushes.Red;
+                line.StrokeThickness = 3;
+
+                line.X1 = xPant;
+                line.Y1 = ypantallaEjeX;
+                line.X2 = xPant;
+                line.Y2 = yPant;
+
+                lineList.Add(line);
+
+            }
+
+            foreach (Line l in lineList)
+            {
+                barCanvas.Children.Add(l);
+            }
+
+
+
+        }
+
         private void MetroWindow_SizeChanged(object sender, SizeChangedEventArgs e)
         {
             createPolylineChart();
+            createBarChart();
         }
     }
 }
