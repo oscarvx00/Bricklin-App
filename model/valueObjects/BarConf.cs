a84814ba4abc7db94a4248881c335a45eda86d47
Oscarvicente@usal.es <Oscarvicente@usal.es>
2020-12-08 14:49:43 +0100

    Singleton

---
+++ model/valueObjects/BarConf.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Bricklin_App.model
+{
+    class BarConf
+    {
+
+        Color foreground = Colors.Red;
+        Color background = Colors.Gray;
+
+        public BarConf()
+        {
+
+        }
+
+        public void setForeground(Color color)
+        {
+            foreground = color;
+        }
+
+        public Color getForeground()
+        {
+            return foreground;
+        }
+
+        public void setBackground(Color color)
+        {
+            background = color;
+        }
+
+        public Color getBackground()
+        {
+            return background;
+        }
+
+    }
+}
---
+++ model/valueObjects/PolylineConf.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Bricklin_App.model
+{
+    class PolylineConf
+    {
+
+        Color foreground = Colors.Red;
+        Color background = Colors.Gray;
+        int stroke = 2;
+
+        public PolylineConf()
+        {
+
+        }
+
+        public void setForeground(Color color)
+        {
+            foreground = color;
+        }
+
+        public Color getForeground()
+        {
+            return foreground;
+        }
+
+        public void setBackground(Color color)
+        {
+            background = color;
+        }
+
+        public Color getBackground()
+        {
+            return background;
+        }
+
+        public void setStroke(int val)
+        {
+            stroke = val;
+        }
+
+        public int getStroke()
+        {
+            return stroke;
+        }
+
+    }
+}
