76f441e497233eb36d4aed0a9fee780c04bfcb89
Oscarvicente@usal.es <Oscarvicente@usal.es>
2020-12-08 18:17:59 +0100

    2020/12/08 1817 Chart Displayed

--- gestionGraficos/ChartsWindow.xaml.cs
+++ gestionGraficos/ChartsWindow.xaml.cs
@@ -23,6 +23,11 @@ namespace Bricklin_App
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+
+        Polyline polyline;
+
+        SortedDictionary<double, double> actualPolylineData = null;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -30,17 +35,23 @@ namespace Bricklin_App
             ThemeManager.Current.ThemeSyncMode = ThemeSyncMode.SyncWithAppMode;
             ThemeManager.Current.SyncTheme();
 
-            
+            polyline = new Polyline();
+ 
             polylineCanvas.Background = Model.getInstance().getPolylineConf().getBackgroundBrush();
+            polyline.Stroke = Model.getInstance().getPolylineConf().getForegroundBrush();
+            polyline.StrokeThickness = Model.getInstance().getPolylineConf().getStroke();
+
             barCanvas.Background = Model.getInstance().getBarConf().getBackgroundBrush();
 
+
+            polylineCanvas.Children.Add(polyline);
 
             //ThemeManager.Current.ChangeTheme(this, "Light.Red");
         }
 
         private void editDataClicked(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
         private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -50,7 +61,8 @@ namespace Bricklin_App
             switch (tabItem)
             {
                 case "Polilinea":
-                    //Polyline chart stuff
+                    actualPolylineData = Model.getInstance().getDataset().getData();
+                    createPolylineChart();
                     break;
                 case "Barras":
                     //Bar chart stuff
@@ -61,5 +73,58 @@ namespace Bricklin_App
             }
         }
 
+        private void createPolylineChart()
+        {
+
+            double padding = 10;
+
+            SortedDictionary<double, double> data = actualPolylineData;
+
+            if (data == null)
+                return;
+
+            int numPuntos = (int)polylineCanvas.ActualWidth;
+            double xReal, yReal, xRealMax, xRealMin, yRealMax, yRealMin;
+
+            xRealMax = data.Keys.Max();
+            xRealMin = data.Keys.Min();
+            yRealMax = data.Values.Max();
+            yRealMin = data.Values.Min();
+
+            double xPant, yPant, xPantMin, xPantMax, yPantMin, yPantMax;
+
+            
+            xPantMin = 0;
+            xPantMax = polylineCanvas.ActualWidth;
+            yPantMin = 0;
+            yPantMax = polylineCanvas.ActualHeight;
+            /*
+            xPantMin = padding;
+            xPantMax = polylineCanvas.ActualWidth - padding;
+            yPantMin = padding;
+            yPantMax = polylineCanvas.ActualHeight - padding;*/
+
+            polyline.Points.Clear();
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                xReal = data.ElementAt(i).Key;
+                yReal = data.ElementAt(i).Value;
+
+                xPant = (xPantMax - xPantMin) * (xReal - xRealMin) / (xRealMax - xRealMin) + xPantMin;
+                yPant = (yPantMin - yPantMax) * (yReal - yRealMin) / (yRealMax - yRealMin) + yPantMax;
+
+
+                Point pt = new Point(xPant, yPant);
+
+                polyline.Points.Add(pt);
+            }
+
+        }
+
+        private void MetroWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            createPolylineChart();
+        }
     }
 }
